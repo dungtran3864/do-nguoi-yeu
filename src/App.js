e011d1694fb8d05acaaa49779a4ee02bc508b726
Duc Phan <ducphanduyagentp@users.noreply.github.com>
2021-12-05 20:58:23 +0700

    Update App.js
    Update the content of App.js to reflect proper Vietnamese text.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import kittenImg from './img/kitten-love.gif';
 function App() {
   return (
     <div className="App">
-      <p className="animate__animated animate__slideInRight animate__infinite">Anh chin nhui, anh yeu em nhat ❤️❤️❤️️</p>
+      <p className="animate__animated animate__slideInRight animate__infinite">Anh chin nhũi, anh yêu em nhất ❤️❤️❤️️</p>
       <img src={kittenImg} alt="cat sorry" width="250" />
     </div>
   );
